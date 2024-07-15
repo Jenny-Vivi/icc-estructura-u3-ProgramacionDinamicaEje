a570c785a330a9bd5793d6f165400a0bba57972e
Viviana Carchi <jcarchif@est.ups.edu.ec>
2024-07-15 17:43:53 -0500

    Programación Dinamica y Ejerccios

--- src/App.java
+++ src/App.java
@@ -1,11 +1,20 @@
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 import Ejercicio.EjercicioDos;
-//import Ejercicio.EjercicioUno;
+import Ejercicio.EjercicioUno;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        EjercicioUno ejerUno = new EjercicioUno();
+        List<Integer> set = new ArrayList<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        List<List<Integer>> result = ejerUno.subsets(set);
+        System.out.println("Resolución del Ejercicio Uno:");
+        System.out.println(result);
 
         EjercicioDos ejerDos = new EjercicioDos();
 
@@ -18,5 +27,6 @@ public class App {
         List<String> result2 = ejerDos.generateParenthesis(n2);
         System.out.println("Resolución del Ejercicio Dos con 3 de entrada:");
         System.out.println(result2);
+
     }
 }
